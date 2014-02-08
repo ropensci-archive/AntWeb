d1dd93d9c3d15645e732bd169e36b5772fb6b84f
Karthik Ram <karthik.ram@gmail.com>
2014-02-07 17:51:38 -0800

    Completed search by coordinates.

---
+++ R/aw_coords.R
@@ -0,0 +1,27 @@
+
+
+#' aw_coords
+#'
+#' Retrieve AntWeb data by location. A radius argument can be supplied as a search radius around a point on th emap.
+#' @param  coord Latitude and Longitude. Should be supplied as \code{lat,long}. Example: 37.76,-122.45
+#' @param  r A radius in kilometers. For 2 km add \code{r = 2.}
+#' @export
+#' @return \code{\link{aw_data}}
+#' @examples \dontrun{
+#' data_by_loc <- aw_coords(coord = "37.76,-122.45", r = 2)
+#'}
+aw_coords <- function( coord = NULL, r = NULL) {
+	assert_that(!is.null(coord))
+	base_url <- "http://www.antweb.org/api/"
+	args <- z_compact(as.list(c(coord = coord, r = r)))
+	results <- GET(base_url, query = args)
+	stop_for_status(results)
+	data <- fromJSON(content(results, "text"))
+	data_list <- lapply(data, function(z) {
+		specimen_by_loc <- data.frame(t(z$meta))
+	})
+	data_df <- do.call(rbind, data_list)
+	data_df$other <- NULL
+	data_df
+}
+
