65f6e734c130b9ea6d8d1ba0a2b8af777f9d90a8
Karthik Ram <karthik.ram@gmail.com>
2014-02-07 18:00:22 -0800

    Added unit tests

---
+++ tests/testthat/test-all.R
@@ -0,0 +1,32 @@
+
+context("Ant Data")
+
+test_that("We are able to retreive all ant data correctly", {
+	ad <- aw_data(genus = "acanthognathus", species = "brevicornis")
+	expect_is(ad, "data.frame")
+	expect_error(aw_data())
+})
+
+context("Data by specimen id works correctly")
+
+test_that("Specimen collections work correctly", {
+	data_by_code <- aw_code(code = "casent0104669")
+	expect_is(data_by_code, "list")
+}) 
+
+
+test_that("We can correctly retrieve data by coordinates", {
+data_by_loc <- aw_coords(coord = "37.76,-122.45", r = 2)
+expect_is(data_by_loc, "data.frame")
+expect_error(aw_coords())
+})
+
+context("Photos")
+
+test_that("Photos work correctly", {
+	z <- aw_photos(since = 5)
+	z1 <- aw_photos(since = 5, type = "d")
+	expect_is(z, "data.frame")
+	expect_is(z1, "data.frame")
+	expect_equal(unique(z1$type), "d")
+})
