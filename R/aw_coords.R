b6f83ec239503c91614bdf94098eef2814a8bd39
Karthik Ram <karthik.ram@gmail.com>
2014-02-08 00:29:05 -0800

    Added additional error handling
    Also, dplyr doesn't always work and the warnings make it impossible to pass CRAN checks.

--- R/aw_coords.R
+++ R/aw_coords.R
@@ -11,7 +11,8 @@
 #' data_by_loc <- aw_coords(coord = "37.76,-122.45", r = 2)
 #'}
 aw_coords <- function( coord = NULL, r = NULL) {
-	assert_that(!is.null(coord))
+	assert_that(!is.null(coord) & is.character(coord))
+
 	base_url <- "http://www.antweb.org/api/"
 	args <- z_compact(as.list(c(coord = coord, r = r)))
 	results <- GET(base_url, query = args)
@@ -21,6 +22,13 @@ aw_coords <- function( coord = NULL, r = NULL) {
 		specimen_by_loc <- data.frame(t(z$meta))
 	})
 	data_df <- do.call(rbind, data_list)
+	# Trying to use dplyr's rbind_all fails here.
+	# data_df <- rbind_all(data_list)
+# 	*** caught segfault ***
+# address 0x0, cause 'memory not mapped'
+
+# Traceback:
+#  1: .Call("dplyr_rbind_all", PACKAGE = "dplyr", dots)
 	data_df$other <- NULL
 	data_df
 }
