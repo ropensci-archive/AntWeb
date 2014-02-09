c50b46324e2c4adb4c26f4a9d2c3412c441e4c1d
Karthik Ram <karthik.ram@gmail.com>
2014-02-08 18:02:32 -0800

    Fixed issue where data.frame contained lists

--- R/aw_coords.R
+++ R/aw_coords.R
@@ -6,6 +6,7 @@
 #' @param coord Latitude and Longitude. Should be supplied as \code{lat,long}. Example: \code{37.76,-122.45}
 #' @param r A radius in kilometers. For 2 km add \code{r = 2}
 #' @export
+#' @importFrom plyr rbind.fill
 #' @return \code{\link{aw_data}}
 #' @examples \dontrun{
 #' data_by_loc <- aw_coords(coord = "37.76,-122.45", r = 2)
@@ -19,16 +20,9 @@ aw_coords <- function( coord = NULL, r = NULL) {
 	stop_for_status(results)
 	data <- fromJSON(content(results, "text"))
 	data_list <- lapply(data, function(z) {
-		specimen_by_loc <- data.frame(t(z$meta))
+		specimen_by_loc <- data.frame(t(unlist(z$meta)))
 	})
-	data_df <- do.call(rbind, data_list)
-	# Trying to use dplyr's rbind_all fails here.
-	# data_df <- rbind_all(data_list)
-# 	*** caught segfault ***
-# address 0x0, cause 'memory not mapped'
-
-# Traceback:
-#  1: .Call("dplyr_rbind_all", PACKAGE = "dplyr", dots)
+	data_df <- do.call(rbind.fill, data_list)
 	data_df$other <- NULL
 	data_df
 }
