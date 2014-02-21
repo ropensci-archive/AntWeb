e72522c60f6e3d513f247f6aa5209064a68f4a8c
Karthik Ram <karthik.ram@gmail.com>
2014-02-21 07:50:25 -0800

    Added error to catch more than 30 species being passed to a map.

--- R/aw_map.R
+++ R/aw_map.R
@@ -27,14 +27,17 @@ aw_map <- function(aw_obj, dest = tempdir(), title = "AntWeb species map", incl.
 	species_data <- aw_obj
 	lat_location <- which(names(species_data) == "decimal_latitude")
 	lon_location <- which(names(species_data) == "decimal_longitude")
+	num_species <- length(unique(species_data$scientific_name))
+		if(num_species > 30) {
+			stop("Map cannot accommodate more than 30 species. Please map a smaller subset")
+	}
 
 	ee_geo <- toGeoJSON(data = species_data, name = "temp", dest = dest, lat.lon = c(lat_location, lon_location))	
-	num_species <- length(unique(species_data$scientific_name))
+
 	cols <- c("#8D5C00", "#2F5CD7","#E91974", "#3CB619","#7EAFCC",
 "#4F2755","#F5450E","#264C44","#3EA262","#FA43C9","#6E8604","#631D0E","#EE8099","#E5B25A",
 "#0C3D8A","#9E4CD3","#195C7B","#9F8450","#7A0666","#BBA3C5","#F064B4","#108223","#553502",
 "#17ADE7","#83C445","#C52817","#626302","#9F9215","#6CCD78","#BF3704")
-
 	pal <- cols[1:num_species]
 	sty <- styleCat(prop = "scientific_name", val = unique(species_data$scientific_name), style.val = pal, fill.alpha = 1, alpha = 1, rad = 4, leg = "Scientific Name")
 	map <- leaflet(ee_geo, base.map="tls", style = sty, popup = "scientific_name", dest = dest, title = title, incl.data = incl.data)
