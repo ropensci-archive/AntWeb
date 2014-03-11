c7372f6b4cbfbb17691676cda9d06354dc7c70ec
Karthik Ram <karthik.ram@gmail.com>
2014-03-10 18:39:54 -0700

    Added print method for antweb class

--- R/aw_utils.R
+++ R/aw_utils.R
@@ -1,2 +1,13 @@
 #' @noRd
 z_compact <- function(l) Filter(Negate(is.null), l)
+
+
+print.antweb <- function(x, ...) {
+cat(sprintf("[Total results on the server]: %s \n", x$count))
+cat("[Args]: \n")
+suppressWarnings(pretty_lists(x$call))
+cat(sprintf("[Limit]: %s \n", x$limit))
+cat(sprintf("[Offset]: %s \n", x$offset))
+cat(sprintf("[Dataset]: [%s x %s] \n[Data preview] :\n", nrow(x$data), ncol(x$data)))
+print(x$data[1:2, ])
+}
