3508eb08801cc83ab3dd024d8c5e83dcab7c2d48
Karthik Ram <karthik.ram@gmail.com>
2014-03-10 18:59:57 -0700

    Now code searches return nicely formatted ant web class results.

--- R/aw_code.R
+++ R/aw_code.R
@@ -20,9 +20,11 @@ aw_code <- function(occurrenceid = NULL) {
 	if(identical(data$specimens$empty_set, "No records found.")) {
 		NULL 
 	} else {
-	data.frame(t(unlist(data[2])))
+	final_df <- data.frame(t(unlist(data[2])))
+	names(final_df)[grep("geojson.coord1", names(final_df))] <- "decimal_latitude"
+	names(final_df)[grep("geojson.coord2", names(final_df))] <- "decimal_longitude"
+	final_results <- list(count = data$count, call = args, data = final_df)
+	class(final_results) <- "antweb"
+	final_results
 }
 }
-# [BUG]
-# Need to coerce dates correctly.
-# Print counts, also add to object. Create a S3 class.
